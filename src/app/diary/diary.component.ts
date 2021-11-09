45395b330aeb1dac66501b48799f5083b2aa939f
Ben Comeau <contact@bencomeau.dev>
2021-11-09 15:59:30 -0800

    Add meals service with mock data

--- src/app/diary/diary.component.ts
+++ src/app/diary/diary.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Meal } from '../meal';
+import { MealService } from '../meal.service';
 
 @Component({
   selector: 'app-diary',
   templateUrl: './diary.component.html',
-  styleUrls: ['./diary.component.css']
+  styleUrls: ['./diary.component.css'],
 })
 export class DiaryComponent implements OnInit {
+  meals: Meal[] = [];
 
-  constructor() { }
+  constructor(private mealService: MealService) {}
 
   ngOnInit(): void {
+    this.getMeals();
   }
 
+  getMeals(): void {
+    this.mealService.getMeals().subscribe((meals) => (this.meals = meals));
+  }
 }
---
+++ src/app/meal.ts
@@ -0,0 +1,8 @@
+export interface Meal {
+  id: number;
+  type: string;
+  name: string;
+  quantity?: string;
+  notes?: string;
+  time: string;
+}
