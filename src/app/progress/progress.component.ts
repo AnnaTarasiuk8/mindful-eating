01c4a4084047116983a7c3c191f4e8c7eb555e2b
Ben Comeau <contact@bencomeau.dev>
2021-12-06 11:03:03 -0800

    Integrate MongoDB public charting to MyProgress

--- src/app/progress/progress.component.ts
+++ src/app/progress/progress.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SocialAuthService } from 'angularx-social-login';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.css'],
 })
 export class ProgressComponent implements OnInit {
-  constructor() {}
+  src?: SafeResourceUrl;
 
-  ngOnInit(): void {}
+  constructor(
+    private socialAuthService: SocialAuthService,
+    public sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit(): void {
+    this.socialAuthService.authState.subscribe((user) => {
+      this.src = this.sanitizer.bypassSecurityTrustResourceUrl(
+        `https://charts.mongodb.com/charts-project-0-yupdi/embed/charts?id=c187812f-e71f-4cfb-a17e-eb52f7b388c2&maxDataAge=10&theme=light&autoRefresh=false&filter={"user_id":%20{$eq:%20${user.id}}}`
+      );
+    });
+  }
 }
